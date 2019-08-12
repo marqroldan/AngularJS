d82e622ee439daf3e51c8a0e69c41bb5e28ebba1
Marq Roldan <hcmlroldan@gmail.com>
2019-08-12 22:30:11 +0800

    Improved history and overflow styling

--- js/app.js
+++ js/app.js
@@ -66,7 +66,61 @@ calculatorApp.controller('calculatorController', ['$scope', '$window', function 
         {value: '00',},
     ]; 
 
+    let _checkDelay = 0;
+
+    let originalFontSize = -1;
+    let lengthOverflow = -1;
+
+    function overflowChecker(reset) {
+        $targetElement = document.querySelector('.inputArea__value');
+        $parentElement = document.querySelector('.inputArea__valueContainer');
+        let curFontSize = parseFloat(getComputedStyle($targetElement)['fontSize'].replace("px",""));
+
+        if(originalFontSize==-1) {
+            originalFontSize = parseFloat(getComputedStyle($targetElement)['fontSize'].replace("px",""));
+        }
+        
+        if(_checkDelay) {
+            _checkDelay = clearTimeout(_checkDelay);
+        }
+
+        _checkDelay = setTimeout(() => {
+
+            if(reset)  {
+                $targetElement.style = '';
+                return;
+            }
+        
+            if($targetElement.scrollHeight >= $parentElement.offsetHeight && curFontSize > 0) {
+                lengthOverflow = $targetElement.textContent.length;
+                curFontSize = curFontSize - (6 * Math.floor($targetElement.scrollHeight / $parentElement.offsetHeight));
+            }
+            else {
+                if(curFontSize < originalFontSize) {
+                    if($targetElement.textContent.length - lengthOverflow <= -5) {
+                        curFontSize = curFontSize + 6;
+                    }
+                    else {
+                        return;
+                    }
+                }
+                else {
+                    return;
+                }
+            }
+
+            $targetElement.style.fontSize = curFontSize + "px";
+            $targetElement = document.querySelector('.inputArea__value');
+
+            _checkDelay = setTimeout(() => {
+                overflowChecker();
+            }, 100);
+            
+        }, 100);
+    }
+
     $scope.buttonClick = function(obj) {
+        let reset = false;
         if($scope.mathString==errorString) $scope.mathString='';
         if(Object.keys(obj).includes('type') && obj.type=="operation") {
             switch (obj.value) {
@@ -77,6 +131,7 @@ calculatorApp.controller('calculatorController', ['$scope', '$window', function 
                     }
                     break;
                 case 'CLC':
+                    reset = true;
                     $scope.mathString = '0';
                     break;
                 case '=':
@@ -88,6 +143,7 @@ calculatorApp.controller('calculatorController', ['$scope', '$window', function 
                         console.log(error);
                     }
                     finally {
+                        reset = true;
                         if($scope.mathString!=errorString) {
                             $scope.histories.push($scope.mathString);
                             $window.localStorage.setItem('histories', JSON.stringify($scope.histories));
@@ -102,5 +158,22 @@ calculatorApp.controller('calculatorController', ['$scope', '$window', function 
             if($scope.mathString=='0' && obj.value!='.') $scope.mathString = '';
             $scope.mathString += obj.value;
         }
+        overflowChecker(reset);
+    }
+
+
+    $scope.checkboxClick = function() {
+        $scope.showType = !$scope.showType;
+        
+        if(!$scope.showType) {
+            setTimeout(() => {
+                overflowChecker();
+            }, 200);
+        }
+    }
+
+    $scope.historyRetrieve = function(string) {
+        $scope.mathString = string;
+        $scope.checkboxClick();
     }
 }]);
